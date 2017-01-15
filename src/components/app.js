84cc797e2706877fc08a6facee320f28450e9eea
Taylor Emolo <tayloremolo@tay.local>
2017-01-15 15:46:42 -0500

    style

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+      
+      <h1>Goody Grabber</h1>
+
+      </div>
+      	
+
+
+ 
     );
   }
 }
